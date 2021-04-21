7fa80c83ad461c64ca7cbaf0db9c02d919abde54
Victor Pindelski <vpindelski@gmail.com>
2021-04-21 16:46:37 +0200

    finish Calculator.asm part 1

--- practical_04/Calculator.asm
+++ practical_04/Calculator.asm
@@ -21,6 +21,12 @@ Main            move.l      #String1,a0
                 move.l      #String3,a0
                 jsr         IsCharError
 
+                move.l      #String4,a0
+                jsr         IsMaxError
+
+                move.l      #String5,a0
+                jsr         IsMaxError
+
                 illegal
 
 ; ===========================
@@ -85,9 +91,65 @@ IsCharError     movem.l      a0/d0,-(a7)
                 rts
 
 ; ===========================
+
+StrLen          movem.l      a0/d1,-(a7)
+                clr.l       d0
+
+\loop           move.b      (a0)+,d1
+                beq         \quit
+
+                add.l       #1,d0
+                bra         \loop
+
+\quit           movem.l     (a7)+,a0/d1
+                rts
+
+IsMaxError      movem.l     a0/d0,-(a7)
+
+                ; comparing the lengths
+                jsr         StrLen
+                cmp.l       #5,d0
+                beq         \equal
+                bhi         \greater
+
+\smaller        and.b       #%11111011,ccr
+                bra         \quit
+
+\greater        or.b        #%00000100,ccr
+                bra         \quit
+
+\equal          cmp.b       #'3',(a0)+
+                bhi         \greater
+                blo         \smaller
+
+                cmp.b       #'2',(a0)+
+                bhi         \greater
+                blo         \smaller                
+
+                cmp.b       #'7',(a0)+
+                bhi         \greater
+                blo         \smaller
+
+                cmp.b       #'6',(a0)+
+                bhi         \greater
+                blo         \smaller
+
+                cmp.b       #'7',(a0)
+                bhi         \greater
+                blo         \smaller
+
+                ; if values are really equal
+                bra         \smaller
+
+\quit           movem.l     (a7)+,a0/d0
+                rts
+
+; ===========================
 ; Data
 ; ===========================
 
 String1         dc.b        "24    + 10",0
 String2         dc.b        "90+ 46   +   2",0
 String3         dc.b        "145b67",0
+String4         dc.b        "32766",0
+String5         dc.b        "32768",0
