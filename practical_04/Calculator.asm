f2414c0c801e291384b10a40fe133fe4e6876f3c
Victor Pindelski <vpindelski@gmail.com>
2021-04-21 16:06:37 +0200

    add IsCharError in Calculator part1 + gitignore file

--- practical_04/Calculator.asm
+++ practical_04/Calculator.asm
@@ -13,10 +13,13 @@ Vector_001      dc.l        Main
                 org         $500
 
 Main            move.l      #String1,a0
-                jsr RemoveSpace
+                jsr         RemoveSpace
 
                 move.l      #String2,a0
-                jsr RemoveSpace2
+                jsr         RemoveSpace2
+
+                move.l      #String3,a0
+                jsr         IsCharError
 
                 illegal
 
@@ -60,7 +63,26 @@ RemoveSpace2    movem.l     d0/a0/a1,-(a7)
 
 ; ===========================
 
-IsCharError
+IsCharError     movem.l      a0/d0,-(a7)
+                
+\loop           move.b      (a0)+,d0
+                beq         \false
+
+                cmp.b       #'0',d0
+                blo         \true
+
+                cmp.b       #'9',d0
+                bhi         \true
+
+                bra         \loop         
+
+\true           or.b        #%00000100,ccr
+                bra         \quit
+
+\false          and.b       #%11111011,ccr
+
+\quit           movem.l     (a7)+,a0/d0
+                rts
 
 ; ===========================
 ; Data
@@ -68,3 +90,4 @@ IsCharError
 
 String1         dc.b        "24    + 10",0
 String2         dc.b        "90+ 46   +   2",0
+String3         dc.b        "145b67",0
